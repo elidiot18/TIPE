19d7c85e18c840c0a418e2351630586e05a3ebe5
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-22 17:33:04 +0100

    Update geometry.h

--- Reconstruction/Algorithmes/3D/geometry.h
+++ Reconstruction/Algorithmes/3D/geometry.h
@@ -9,10 +9,23 @@ typedef struct {
     double x;
     double y;
     double z;
-} Point;
+} PointDouble;
 
-double distance(size_t, size_t, vector<Point>&);
-double distance(size_t, vector<size_t>&, vector<Point>&);
-size_t farthest(vector<Point>&, vector<size_t>&);
+typedef struct {
+    size_t p1;
+    size_t p2;
+    size_t p3;
+} Neighbourhood;
+
+typedef struct {
+    vector<size_t> points;
+    vector< vector<size_t> > edges;
+    vector <vector< vector<size_t> > > triangles;
+} SimplicialComplex;
+
+void init_cwl(SimplicialComplex*, size_t);
+double distance(size_t, size_t, vector<PointDouble>&);
+double distance(size_t, vector<size_t>&, vector<PointDouble>&);
+size_t farthest(vector<PointDouble>&, vector<size_t>&);
 
 #endif // INCLUDE_GEOMETRY
