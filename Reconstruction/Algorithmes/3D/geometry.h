2047da8b6bf42a394fbe0d2f7d303b0d9484058d
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-22 22:41:54 +0100

    Update geometry.h

--- Reconstruction/Algorithmes/3D/geometry.h
+++ Reconstruction/Algorithmes/3D/geometry.h
@@ -2,6 +2,7 @@
 #define INCLUDE_GEOMETRY
 
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -11,21 +12,52 @@ typedef struct {
     double z;
 } PointDouble;
 
-typedef struct {
-    size_t p1;
-    size_t p2;
-    size_t p3;
-} Neighbourhood;
+typedef size_t Point;
 
 typedef struct {
-    vector<size_t> points;
-    vector< vector<size_t> > edges;
-    vector <vector< vector<size_t> > > triangles;
+    Point p1;
+    Point p2;
+} Edge;
+
+typedef struct {
+    Point p1;
+    Point p2;
+    Point p3;
+} Triangle;
+
+struct Edge_less {
+    operator()(Edge e1, Edge e2) {
+        return e1.p1 < e2.p1 || (e1.p1 == e2.p1 && e1.p2 < e2.p2);
+    }
+};
+
+struct Triangle_less {
+    operator()(Triangle t1, Triangle t2) {
+        return t1.p1 < t2.p1 || (t1.p1 == t2.p1 && t1.p2 < t2.p2) || (t1.p1 == t2.p1 && t1.p2 == t2.p2 && t1.p3 < t2.p3);
+    }
+};
+
+typedef struct {
+    set<Point> points;
+    set<Edge, Edge_less> edges;
+    set<Triangle, Triangle_less> triangles;
 } SimplicialComplex;
 
+typedef struct {
+    Point p1;
+    Point p2;
+    Point p3;
+} Neighbourhood;
+
+struct Neighbourhood_less {
+    operator()(Neighbourhood n1, Neighbourhood n2) {
+        return n1.p1 < n2.p1 || (n1.p1 == n2.p1 && n1.p2 < n2.p2) || (n1.p1 == n2.p1 && n1.p2 == n2.p2 && n1.p3 < n2.p3);
+    }
+};
+
 void init_cwl(SimplicialComplex*, size_t);
-double distance(size_t, size_t, vector<PointDouble>&);
-double distance(size_t, vector<size_t>&, vector<PointDouble>&);
-size_t farthest(vector<PointDouble>&, vector<size_t>&);
+double distance(Point, Point, vector<PointDouble>&);
+double distance(Point, vector<size_t>&, vector<PointDouble>&);
+Point farthest(vector<PointDouble>&, vector<Point>&);
 
 #endif // INCLUDE_GEOMETRY
