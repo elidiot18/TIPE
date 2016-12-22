aab21773c56b6b35a42ca421c0603e0d6871f23d
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-22 17:35:10 +0100

    Update reconstruction.h

--- Reconstruction/Algorithmes/3D/reconstruction.h
+++ Reconstruction/Algorithmes/3D/reconstruction.h
@@ -9,15 +9,15 @@
 
 using namespace std;
 
-bool is_witnessed(size_t, size_t, vector<Point>&, vector< vector<size_t> >&);
-bool is_witnessed(size_t, size_t, size_t, vector<Point>&, vector< vector<size_t> >&);
-bool in(size_t, size_t, vector< vector< vector< vector<size_t> > > >&);
-bool in(size_t, size_t, size_t, vector< vector< vector< vector<size_t> > > >&);
-void reconstruction(vector<Point>&, ofstream&);
-void take_out(size_t, size_t, vector< vector< vector< vector<size_t> > > >*);
-void take_out(size_t, size_t, size_t, vector< vector< vector< vector<size_t> > > >*);
-void add_cwl(size_t, size_t, vector< vector< vector< vector<size_t> > > >*);
-void add_cwl(size_t, size_t, size_t k, vector< vector< vector< vector<size_t> > > >*);
-void add_rvl(size_t, vector<size_t>*);
+bool in(size_t, size_t, size_t, SimplicialComplex&);
+bool in(size_t, size_t, SimplicialComplex&);
+bool in(size_t, SimplicialComplex&);
+void del(size_t, size_t, size_t, SimplicialComplex*);
+void del(size_t, size_t, SimplicialComplex*);
+void del(size_t, SimplicialComplex*);
+void add(size_t, size_t, size_t, SimplicialComplex*);
+void add(size_t, size_t, SimplicialComplex*);
+void add(size_t, SimplicialComplex*);
+void del(size_t, vector<size_t>*);
 
 #endif // INCLUDE_RECONSTRUCTION
