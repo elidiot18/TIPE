5c21cfbbbce77f8f870a76d100a5bb9ad00fbe33
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-22 22:41:16 +0100

    Update geometry.cpp

--- Reconstruction/Algorithmes/3D/geometry.cpp
+++ Reconstruction/Algorithmes/3D/geometry.cpp
@@ -1,15 +1,10 @@
 #include "geometry.h"
 
-void init_cwl(SimplicialComplex *CWL, size_t n) {
-    CWL->edges = vector< vector<size_t> >(n);
-    CWL->triangles = vector< vector< vector<size_t> > >(n);
-}
-
-double distance(size_t p1, size_t p2, vector<PointDouble> &W) {
+double distance(Point p1, Point p2, vector<PointDouble> &W) {
     return (W[p1].x - W[p2].x) * (W[p1].x - W[p2].x) + (W[p1].y - W[p2].y) * (W[p1].y - W[p2].y) + (W[p1].z - W[p2].z) * (W[p1].z - W[p2].z);
 }
 
-double distance(size_t p, vector<size_t> &L, vector<PointDouble> &W) {
+double distance(Point p, vector<Point> &L, vector<PointDouble> &W) {
     size_t l = L.size();
     double d = distance(p, L[0], W);
     for (size_t k = 0; k < l; ++k) {
@@ -21,7 +16,7 @@ double distance(size_t p, vector<size_t> &L, vector<PointDouble> &W) {
     return d;
 }
 
-size_t farthest(vector<PointDouble> &W, vector<size_t> &L) {
+size_t farthest(vector<PointDouble> &W, vector<Point> &L) {
     size_t n = W.size();
     size_t m = 0;
     double dm = distance(m, L, W);
