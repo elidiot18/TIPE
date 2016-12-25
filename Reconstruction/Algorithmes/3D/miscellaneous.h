ef1029d29c791dbb37ef0c5eb48d5bbbba7a66a7
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-25 18:49:52 +0100

    Update miscellaneous.h

--- Reconstruction/Algorithmes/3D/miscellaneous.h
+++ Reconstruction/Algorithmes/3D/miscellaneous.h
@@ -1,12 +1,11 @@
 #ifndef INCLUDE_MISCELLANEOUS
 #define INCLUDE_MISCELLANEOUS
 
+#include "geometry.h"
 #include <string>
 
 using namespace std;
 
 string remove_extension(string&);
-void order(Point*, Point*, Point*);
-void order(Point*, Point*);
 
 #endif // INCLUDE_MISCELLANEOUS
