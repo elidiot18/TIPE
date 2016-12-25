42b7d6856ac135c9e91482320d22736c11e68d77
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-25 18:49:35 +0100

    Update miscellaneous.cpp

--- Reconstruction/Algorithmes/3D/miscellaneous.cpp
+++ Reconstruction/Algorithmes/3D/miscellaneous.cpp
@@ -1,25 +1,8 @@
 #include "miscellaneous.h"
+#include "geometry.h"
 
 string remove_extension(string &s) {
     size_t lastdot = s.find_last_of('.');
     if (lastdot == string::npos) return s;
     return s.substr(0, lastdot);
-}
-
-void order(Point *i, Point *j, Point *k) {
-    Point ii, jj, kk;
-    ii = min(min(*i, *j), *k);
-    kk = max(max(*i, *j), *k);
-    jj = *i + *j + *k - ii - kk;
-    *i = ii;
-    *j = jj;
-    *k = kk;
-}
-
-void order(Point *i, Point *j) {
-    Point ii, jj;
-    ii = min(*i, *j);
-    jj = max(*i, *j);
-    *i = ii;
-    *j = jj;
 }
