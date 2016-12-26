d30df2efb83141ff0a2fcfcc954beee51e7e147c
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-26 23:48:13 +0100

    Update geometry.h

--- Reconstruction/Algorithmes/3D/geometry.h
+++ Reconstruction/Algorithmes/3D/geometry.h
@@ -2,9 +2,11 @@
 #define INCLUDE_GEOMETRY
 
 #include <vector>
+#include <map>
 #include <set>
 #include <limits>
 #include <cmath>
+#include <memory>
 
 struct Coordinates;
 struct Point;
@@ -12,11 +14,11 @@ struct Edge;
 struct Triangle;
 
 double distance(const Coordinates&, const Coordinates&);
-double distance(const Point&, const Point&);
-double distance(const Point&, const std::vector<Point*>&);
-Point farthest(const std::vector<Point>&, const std::vector<Point*>&);
-Edge get_edge(const Point&, const Point&);
-Triangle get_triangle(const Point&, const Point&, const Point&);
+double distance(const Point*, const Point&);
+double distance(const Point*, const std::vector<Point*>&);
+Point* farthest(const std::vector<Point*>&, const std::vector<Point*>&);
+Edge get_edge(Point*, Point*);
+Triangle get_triangle(Point*, Point*, Point*);
 
 struct Coordinates {
     double x;
@@ -27,54 +29,35 @@ struct Coordinates {
 struct SimplicialComplex;
 
 struct Point {
-    static Coordinates coordinates;
+    Coordinates coordinates;
     size_t index;
-
-    struct Compare {
-        bool operator()(Point* lhs, Point* rhs) {
-            return distance(lhs->coordinates, Point::coordinates) < distance(rhs->coordinates, Point::coordinates);
-        }
-    };
-
     bool operator<(const Point& rhs) const {return this->index < rhs.index;}
     bool operator==(const Point& rhs) const {return this->index == rhs.index;}
 
-    bool operator<(Point* rhs) const {return this->index < rhs->index;}
-    bool operator==(Point* rhs) const {return this->index == rhs->index;}
+    Point(size_t index, double x, double y, double z) {this->index = index; coordinates.x = x; coordinates.y = y; coordinates.z =z;}
 
-    std::multiset<Point*, Compare> neighbourhood;
-
-    SimplicialComplex* simplices;
+    std::multimap<double, Point*> neighbourhood;
+    std::unique_ptr<SimplicialComplex> simplices;
 };
 
 struct Edge {
-    Edge(Point const *, Point const *);
-    Point const * p1;
-    Point const * p2;
+    Edge(Point*, Point*);
+    bool operator<(const Edge& e) const {return *e.p1 < *this->p1 || (*e.p1 == *this->p1 && *e.p2 < *this->p2);}
+    Point* p1;
+    Point* p2;
 };
 
 struct Triangle {
-    Triangle(Point const *, Point const *, Point const *);
-    Point const * p1;
-    Point const * p2;
-    Point const * p3;
-};
-
-struct Edge_less {
-    bool operator()(const Edge& e1, const Edge& e2) {
-        return e1.p1 < e2.p1 || (e1.p1 == e2.p1 && e1.p2 < e2.p2);
-    }
-};
-
-struct Triangle_less {
-    bool operator()(Triangle t1, Triangle t2) {
-        return t1.p1 < t2.p1 || (t1.p1 == t2.p1 && t1.p2 < t2.p2) || (t1.p1 == t2.p1 && t1.p2 == t2.p2 && t1.p3 < t2.p3);
-    }
+    Triangle(Point*, Point*, Point*);
+    bool operator<(const Triangle& t) const {return *t.p1 < *this->p1 || (*t.p1 == *this->p1 && *t.p2 < *this->p2) || (*t.p1 == *this->p1 && *t.p2 == *this->p2 && *t.p3 < *this->p3);}
+    Point* p1;
+    Point* p2;
+    Point* p3;
 };
 
 struct SimplicialComplex {
-    std::set<Edge, Edge_less> edges;
-    std::set<Triangle, Triangle_less> triangles;
+    std::set<Edge> edges;
+    std::set<Triangle> triangles;
 };
 
 #endif // INCLUDE_GEOMETRY
