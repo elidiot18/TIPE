f322bc098c3a5a3bf6663a5173e00c9d735c71a1
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-26 23:47:57 +0100

    Update geometry.cpp

--- Reconstruction/Algorithmes/3D/geometry.cpp
+++ Reconstruction/Algorithmes/3D/geometry.cpp
@@ -2,20 +2,16 @@
 
 using namespace std;
 
-double distance(const Point& p1, const Point& p2) {
-    const Coordinates& c1 = p1.coordinates;
-    const Coordinates& c2 = p2.coordinates;
+double distance(const Point* p1, const Point* p2) {
+    const Coordinates& c1 = p1->coordinates;
+    const Coordinates& c2 = p2->coordinates;
     return pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2) + pow(c1.z - c2.z, 2);
 }
 
-double distance(const Coordinates& c1, const Coordinates& c2) {
-    return pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2) + pow(c1.z - c2.z, 2);
-}
-
-double distance(const Point& p, const vector<Point*>& L) {
+double distance(const Point* p, const vector<Point*>& L) {
     double d = numeric_limits<double>::max();
     for (Point* l : L) {
-        double dl = distance(p, *l);
+        double dl = distance(p, l);
         if (dl < d) {
             d = dl;
         }
@@ -23,23 +19,23 @@ double distance(const Point& p, const vector<Point*>& L) {
     return d;
 }
 
-Point farthest(const vector<Point>& W, const vector<Point*>& L) {
-    const Point* out;
+Point* farthest(const vector<Point*>& W, const vector<Point*>& L) {
+    Point* out;
     double dm(0);
-    for (const Point& p : W) {
+    for (Point* p : W) {
         double d = distance(p, L);
         if (d > dm) {
             dm = d;
-            out = &p;
+            out = p;
         }
     }
-    return *out;
+    return out;
 }
 
-Triangle::Triangle(Point const * i, Point const * j, Point const * k) {
-    if (i < j) {
-        if (i < k) {
-            if (j < k) {
+Triangle::Triangle(Point* i, Point* j, Point* k) {
+    if (*i < *j) {
+        if (*i < *k) {
+            if (*j < *k) {
                 p1 = i; p2 = j; p3 = k;
             }
             else {
@@ -51,8 +47,8 @@ Triangle::Triangle(Point const * i, Point const * j, Point const * k) {
         }
     }
     else {
-        if (j < k) {
-            if (i < k) {
+        if (*j < *k) {
+            if (*i < *k) {
                 p1 = j; p2 = i; p3 = k;
             }
             else {
@@ -65,6 +61,6 @@ Triangle::Triangle(Point const * i, Point const * j, Point const * k) {
     }
 }
 
-Edge::Edge(Point const * i, Point const * j) {
+Edge::Edge(Point* i, Point* j) {
     i < j ? (p1 = i, p2 = j) : (p1 = j, p2 = i);
 }
