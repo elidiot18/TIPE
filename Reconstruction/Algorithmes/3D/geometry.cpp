4afacdd0c4bd6af908d61651ea59f3862dabbfca
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-27 19:24:38 +0100

    Update geometry.cpp

--- Reconstruction/Algorithmes/3D/geometry.cpp
+++ Reconstruction/Algorithmes/3D/geometry.cpp
@@ -1,53 +1,59 @@
-#include "geometry.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "reconstruction.cpp"
+#include "geometry.cpp"
+#include "miscellaneous.cpp"
 
 using namespace std;
 
-double distance(const Point* p1, const Point* p2) {
-    const Coordinates& c1 = p1->coordinates;
-    const Coordinates& c2 = p2->coordinates;
-    return pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2) + pow(c1.z - c2.z, 2);
-}
-
-double distance(const Point* p, const vector<Point*>& L) {
-    double d = numeric_limits<double>::max();
-    for (Point* l : L) {
-        double dl = distance(p, l);
-        if (dl < d) {
-            d = dl;
-        }
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        cout << "You need to specify a file";
     }
-    return d;
-}
-
-Point* farthest(const vector<Point*>& W, const vector<Point*>& L) {
-    Point* out;
-    double dm(0);
-    for (Point* p : W) {
-        double d = distance(p, L);
-        if (d > dm) {
-            dm = d;
-            out = p;
-        }
+    if (argc > 2) {
+        cout << "There is only one argument to be specified";
     }
-    return out;
-}
-
-Triangle::Triangle(const Point* i, const Point* j, const Point* k) {
-    vector<size_t> vec = {i->index, j->index, k->index};
-    sort(vec.begin(), vec.end());
-    this->index1 = vec[0];
-    this->index2 = vec[1];
-    this->index3 = vec[2];
-}
-
-Edge::Edge(const Point* i, const Point* j) {
-    size_t iindex = i->index, jindex = j->index;
-    if (iindex < jindex) {
-        this->index1 = iindex;
-        this->index2 = jindex;
+    ifstream ifile;
+    ifile.open(argv[1], ios::in);
+    if (!ifile.is_open()) {
+        return -1;
     }
-    else {
-        this->index1 = jindex;
-        this->index2 = iindex;
+    string name = argv[1];
+    name = remove_extension(name);
+    name = name + (string)".cplx";
+    ofstream ofile(name.c_str(), ios::out | ios::trunc);
+    if (!ofile.is_open()) {
+        return -1;
     }
+
+    string dim;
+    getline(ifile, dim);
+    if (dim != "3") {
+        cout << "This algorithm does only work for dimension 3";
+        return 42;
+    }
+
+    vector<Point> W;
+
+    size_t i = 0;
+    while (ifile) {
+        double x, y, z;
+        ifile >> x;
+        ifile >> y;
+        ifile >> z;
+        W.emplace_back(Point(i, x, y, z));
+        ++i;
+    }
+
+    vector<Point*> _W;
+    for (Point& w : W)
+        _W.push_back(&w);
+
+    reconstruction(_W, ofile);
+    ifile.close();
+    ofile.close();
+    return 0;
 }
