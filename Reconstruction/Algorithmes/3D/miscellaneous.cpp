39558ab956cb950485a1cd1c7ad7a25d9466e2ea
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-29 20:39:05 +0100

    Code beautified

--- Reconstruction/Algorithmes/3D/miscellaneous.cpp
+++ Reconstruction/Algorithmes/3D/miscellaneous.cpp
@@ -1,7 +1,8 @@
 #include "miscellaneous.h"
 #include "geometry.h"
 
-string remove_extension(string &s) {
+string remove_extension(string &s)
+{
     size_t lastdot = s.find_last_of('.');
     if (lastdot == string::npos) return s;
     return s.substr(0, lastdot);
