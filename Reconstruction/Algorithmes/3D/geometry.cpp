47964cc76b0dd9c7df60e2a60df2098bb7e7d406
Pierre ELIS <pierre.elis@hotmail.fr>
2016-12-29 18:45:56 +0100

    Update geometry.cpp

--- Reconstruction/Algorithmes/3D/geometry.cpp
+++ Reconstruction/Algorithmes/3D/geometry.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-double distance(const Point* p1, const Point* p2) {
+double _distance(const Point* p1, const Point* p2) {
     const Coordinates& c1 = p1->coordinates;
     const Coordinates& c2 = p2->coordinates;
     return pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2) + pow(c1.z - c2.z, 2);
@@ -11,7 +11,7 @@ double distance(const Point* p1, const Point* p2) {
 double distance(const Point* p, const vector<Point*>& L) {
     double d = numeric_limits<double>::max();
     for (Point* l : L) {
-        double dl = distance(p, l);
+        double dl = _distance(p, l);
         if (dl < d) {
             d = dl;
         }
@@ -32,7 +32,7 @@ Point* farthest(const vector<Point*>& W, const vector<Point*>& L) {
     return out;
 }
 
-Triangle::Triangle(const Point* i, const Point* j, const Point* k) {
+Triangle::Triangle(Point* i, Point* j, Point* k) {
     vector<size_t> vec = {i->index, j->index, k->index};
     sort(vec.begin(), vec.end());
     this->index1 = vec[0];
@@ -40,7 +40,7 @@ Triangle::Triangle(const Point* i, const Point* j, const Point* k) {
     this->index3 = vec[2];
 }
 
-Edge::Edge(const Point* i, const Point* j) {
+Edge::Edge(Point* i, Point* j) {
     size_t iindex = i->index, jindex = j->index;
     if (iindex < jindex) {
         this->index1 = iindex;
